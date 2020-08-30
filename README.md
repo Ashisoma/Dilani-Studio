f4b9b878c59453328d3cb76f56e0e2c55cf59ae2
Ashisoma <okalionesmus@gmail.com>
2020-08-30 22:58:20 +0300

    add edits to jquery

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# Delani Studio
+### A website that
+
+#### By **Onesmus Okali**
+
+# Description
+
+# SetUp/Installation Requirements
+To visit our website and check out our designs, just follow the simple steps below:
+1. Make sure you are connected to the internet.
+2. Open your web browser,
+3. Finally click on this link https://ashisoma.github.io/Dilani-Studio/
+
+## Technology Used
+The programminglanguages used to build this website are HTML and css fpr tyhe front end. For back-end, jQUERY was used and alos to make the web page interactive
 
--- js/script.js
+++ js/script.js
@@ -1,39 +1,36 @@
 $(document).ready(function(){
-    $("img#design").click(function(){
+    $("#design").click(function(){
         $("#p1-hide").show()
         $("#design").hide()
-    },function(){
-        $("#p1-hide").click(function(){
-            $("#design").show()
-            $("#p1-hide").hide()
-        })
-    });
-    $("#p2-hide").click(function(){
-        $("#p2-hide").show()
-        $("#devpt").hide()
-
-    });
-        $("#p3-hide").click(function(){
-            $("#p3-hide").show()
-            $("#project").hide()    
     });
 });
+
+$(document).ready(function(){
+    $("#devpt").click(function(){
+        $("#p2-hide").show()
+        $("#devpt").hide()
+    });
+});
+$(document).ready(function(){
+    $("#project").click(function(){
+        $("#p3-hide").show()
+        $("#project").hide()
+    });
+});
+
  
 
   
-$(document).ready(function(){
-    var name = parseInt("#name");
+$(document).ready(function getResponse(){
+    var name = document.getElementById("#name").value();
     $("#submit").click(function(){
-    var name = 
-
         alert("Hi, " + name + " .Your message has been received!")
     })
  
 })
 
 $(document).ready(function(){
-    $("#images").hover(function(){
-        var see = ["p1-hide", "p2-hide", "p3-hide"]
+    $("#row").hover(function(){
     
     })
 })
